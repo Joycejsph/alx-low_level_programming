b6ec611fcdfa4c67f514f47b85abda1a25ccf482
Joycejsph <Joycealuko2018@gmail.com>
2022-09-16 17:32:14 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - program that prints either number 
+*or fizz or buzz or fizzbuzz
+*
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num %3 == 0) && (num % 5 == 0))
+		{
+			print("fizzbuzz ");
+		;
+		else if ((num % 3) == 0)
+		}
+			printf("fizz");
+		}
+		else if ((num % 5) -- 0)
+		{
+			if (num i= 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
