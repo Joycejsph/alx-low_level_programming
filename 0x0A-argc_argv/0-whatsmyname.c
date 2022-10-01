3709304749c6a351744a82f310660b4f7b7071bb
Joycejsph <Joycealuko2018@gmail.com>
2022-09-30 18:41:32 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,8 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-
-	printf("%s\n", argv[argc * 0]);
-
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
