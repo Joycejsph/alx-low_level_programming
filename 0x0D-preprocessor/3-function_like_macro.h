3bb9298249052bf9e79d07b2831ec5cdce93b177
Joycejsph <Joycealuko2018@gmail.com>
2022-10-10 18:05:18 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,10 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+/*
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
-int main(void)
-{
-    int i;
-    int j;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
-}
+#define ABS(x) ((x) < 0 ? (-x) : (x))
